158a336faeb182d563a310bb360db84d25020ebd
skoegletestproject <mail@manojgowda.in>
2025-01-24 16:59:18 +0530

    sdfdsjha

--- src/Layout/Footor.jsx
+++ src/Layout/Footor.jsx
@@ -1,26 +1,25 @@
 import React from "react";
-
 import Box from "@mui/material/Box";
-
 import Typography from "@mui/material/Typography";
 
 const Footer = () => {
-    return (
-      <Box
-        component="footer"
-        sx={{
-          py: 2,
-          px: 2,
-          mt: 'auto',
-          textAlign: 'center',
-          backgroundColor: theme => theme.palette.primary.main,
-          color: 'white',
-        }}
-      >
-        <Typography variant="body2">
-          © {new Date().getFullYear()} skoegle. All rights reserved.
-        </Typography>
-      </Box>
-    );
-  };
-export default Footer
+  return (
+    <Box
+      component="footer"
+      sx={{
+        py: 2,
+        px: 2,
+        mt: 'auto',
+        textAlign: 'center',
+        backgroundColor: '#00796b', // Matching green color with the navbar
+        color: 'white',
+      }}
+    >
+      <Typography variant="body2">
+        © {new Date().getFullYear()} skoegle. All rights reserved.
+      </Typography>
+    </Box>
+  );
+};
+
+export default Footer;
